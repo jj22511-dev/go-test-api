ffc8670eecfdf97c0c22c7ba210fab9960f6e58d
jj22511-dev <jj22511@gmail.com>
2024-04-20 09:48:11 +0800

    create a simple endpoint

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Request Multiplexer
+	mux := http.NewServeMux()
+
+	// Register Routes
+	mux.Handle("/home", &homeHandler{})
+
+	// Run Server
+	http.ListenAndServe(":5000", mux)
+}
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("This is my home page"))
 }
